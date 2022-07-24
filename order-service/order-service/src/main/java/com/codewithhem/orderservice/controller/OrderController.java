5d09be334e67dad14ca3865ce144246a299997eb
hemant091 <hvaidya5685@gmail.com>
2022-07-24 18:49:52 +0530

    Modified microservices Order-service and Order-service-consumer for gateway integration

--- order-service/order-service/src/main/java/com/codewithhem/orderservice/controller/OrderController.java
+++ order-service/order-service/src/main/java/com/codewithhem/orderservice/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/order")
+@RequestMapping(value = "app/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
